1744b1a0eab2a605e08b896ddaf1583ba1bec33f
Shane Devane <shanedevane@gmail.com>
2021-03-09 08:56:04 +0100

    v1.1 bump

--- trusted_email_validator/__init__.py
+++ trusted_email_validator/__init__.py
@@ -1,6 +1,6 @@
 from trusted_email_validator.trusted_email_validator import TrustedEmailValidator
 
-__version__ = '1'
+__version__ = '1.1'
 __license__ = 'GPL'
 __author__ = 'Shane Devane'
 __email__ = 'shanedevane@gmail.com'
